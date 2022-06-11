7423f19237ef6f922699457d19bd1c16e525544b
mdsajedul <sajedulislms@gmail.com>
2022-06-12 00:20:56 +0600

    product-model

---
+++ Server/Model/productModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    shopId:{
+        type: String,
+        required: true
+    },
+    shopName:{
+        type: String,
+        required: true
+    },
+    sellerId:{
+        type: String,
+        required: true
+    },
+    price:{
+        type: Number,
+        requied: true
+    },
+    productDetail:{
+        type: String
+    },
+    totalRating:{
+        type: Number
+    },
+    reviews:[{
+        review: String, rating: Number, reviewerId: String
+    }],
+    quantity: Number,
+    features:[{
+        type: String
+    }],
+    pictures:[{
+        type: Buffer
+    }],
+    category:[{
+        type: String
+    }]
+
+})
+
+const Product = mongoose.Model('Product',ProductSchema)
+
+module.exports = Product
